7e173c4ad1448c30964c51cfa9f65a8d68552fc0
Ed Carney <carneyedwardj@gmail,com>
2022-08-21 12:06:07 -0400

    Reorganize and create initial makefile

--- Anytime_RRT_Sharp/src/RRT_Sharp.cpp
+++ Anytime_RRT_Sharp/src/RRT_Sharp.cpp
@@ -1,10 +1,4 @@
-#include "configspaceGraph.h"
-#include "workspaceGraph.h"
-#include <tuple>
-
-#ifndef M_PI
-    #define M_PI 3.14159265358979323846
-#endif
+#include "RRT_Sharp.h"
 
 using namespace std;
 
