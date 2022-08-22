bba718cbffcc8897b050bded7485c97ddf6e8ba3
Ed Carney <carneyedwardj@gmail.com>
2022-08-22 08:05:08 -0400

    Additional tests

--- Anytime_RRT_Sharp/src/unitTests.cpp
+++ Anytime_RRT_Sharp/src/unitTests.cpp
@@ -105,6 +105,80 @@ TEST(ARRTS_Obstacles, AddMultipleObstacle_CheckVals)
     GTEST_ASSERT_EQ(obs3.radius, 9);
 }
 
+TEST(ARRTS_Obstacles, AddSingleThenSingleObstacle_CheckNum)
+{
+    ArrtsService service;
+    service.AddObstacle(1, 2, 3);
+    service.AddObstacle(1, 2, 3);
+    GTEST_ASSERT_EQ(service.GetNumObstacles(), 2);
+}
+
+TEST(ARRTS_Obstacles, AddSingleThenSingleObstacle_CheckVals)
+{
+    ArrtsService service;
+    service.AddObstacle(1, 2, 3);
+    service.AddObstacle(4, 5, 6);
+
+    Obstacle obs1 = service.GetObstacle(0);
+    Obstacle obs2 = service.GetObstacle(1);
+
+    GTEST_ASSERT_EQ(obs1.x, 1);
+    GTEST_ASSERT_EQ(obs2.x, 4);
+
+    GTEST_ASSERT_EQ(obs1.y, 2);
+    GTEST_ASSERT_EQ(obs2.y, 5);
+
+    GTEST_ASSERT_EQ(obs1.radius, 3);
+    GTEST_ASSERT_EQ(obs2.radius, 6);
+}
+
+TEST(ARRTS_Obstacles, AddMultipleThenMultipleObstacle_CheckNum)
+{
+    ArrtsService service;
+    const double x1[] = { 1, 2 };
+    const double y1[] = { 1, 2 };
+    const double r1[] = { 1, 2 };
+    const double x2[] = { 1, 2 };
+    const double y2[] = { 1, 2 };
+    const double r2[] = { 1, 2 };
+    service.AddObstacles(x1, y1, r1, 2);
+    service.AddObstacles(x2, y2, r2, 2);
+    GTEST_ASSERT_EQ(service.GetNumObstacles(), 4);
+}
+
+TEST(ARRTS_Obstacles, AddMultipleThenMultipleObstacle_CheckVals)
+{
+    ArrtsService service;
+    const double x1[] = { 1, 2 };
+    const double y1[] = { 3, 4 };
+    const double r1[] = { 5, 6 };
+    const double x2[] = { 7, 8 };
+    const double y2[] = { 9, 10 };
+    const double r2[] = { 11, 12 };
+    service.AddObstacles(x1, y1, r1, 2);
+    service.AddObstacles(x2, y2, r2, 2);
+    
+    Obstacle obs1 = service.GetObstacle(0);
+    Obstacle obs2 = service.GetObstacle(1);
+    Obstacle obs3 = service.GetObstacle(2);
+    Obstacle obs4 = service.GetObstacle(3);
+
+    GTEST_ASSERT_EQ(obs1.x, 1);
+    GTEST_ASSERT_EQ(obs2.x, 2);
+    GTEST_ASSERT_EQ(obs3.x, 7);
+    GTEST_ASSERT_EQ(obs4.x, 8);
+
+    GTEST_ASSERT_EQ(obs1.y, 3);
+    GTEST_ASSERT_EQ(obs2.y, 4);
+    GTEST_ASSERT_EQ(obs3.y, 9);
+    GTEST_ASSERT_EQ(obs4.y, 10);
+
+    GTEST_ASSERT_EQ(obs1.radius, 5);
+    GTEST_ASSERT_EQ(obs2.radius, 6);
+    GTEST_ASSERT_EQ(obs3.radius, 11);
+    GTEST_ASSERT_EQ(obs4.radius, 12);
+}
+
 TEST(ARRTS_Obstacles, AddSingleThenMultipleObstacle_CheckNum)
 {
     ArrtsService service;
@@ -254,6 +328,68 @@ TEST(ARRTS_VehiclePoints, AddMultiplePoint_CheckVals)
     GTEST_ASSERT_EQ(n1.y, 4);
     GTEST_ASSERT_EQ(n2.y, 5);
     GTEST_ASSERT_EQ(n3.y, 6);
+}
+
+TEST(ARRTS_VehiclePoints, AddSingleThenSinglePoint_CheckNum)
+{
+    ArrtsService service;
+    service.AddVehiclePoint(1, 2);
+    service.AddVehiclePoint(1, 2);
+    GTEST_ASSERT_EQ(service.GetNumVehiclePoints(), 2);
+}
+
+TEST(ARRTS_VehiclePoints, AddSingleThenSinglePoint_CheckVals)
+{
+    ArrtsService service;
+    service.AddVehiclePoint(1, 2);
+    service.AddVehiclePoint(3, 4);
+
+    Node n1 = service.GetVehiclePoint(0);
+    Node n2 = service.GetVehiclePoint(1);
+
+    GTEST_ASSERT_EQ(n1.x, 1);
+    GTEST_ASSERT_EQ(n2.x, 3);
+
+    GTEST_ASSERT_EQ(n1.y, 2);
+    GTEST_ASSERT_EQ(n2.y, 4);
+}
+
+TEST(ARRTS_VehiclePoints, AddMultipleThenMultiplePoint_CheckNum)
+{
+    ArrtsService service;
+    const double x1[] = { 1, 2 };
+    const double y1[] = { 1, 2 };
+    const double x2[] = { 1, 2 };
+    const double y2[] = { 1, 2 };
+    service.AddVehiclePoints(x1, y1, 2);
+    service.AddVehiclePoints(x2, y2, 2);
+    GTEST_ASSERT_EQ(service.GetNumVehiclePoints(), 4);
+}
+
+TEST(ARRTS_VehiclePoints, AddMultipleThenMultiplePoint_CheckVals)
+{
+    ArrtsService service;
+    const double x1[] = { 1, 2 };
+    const double y1[] = { 3, 4 };
+    const double x2[] = { 5, 6 };
+    const double y2[] = { 7, 8 };
+    service.AddVehiclePoints(x1, y1, 2);
+    service.AddVehiclePoints(x2, y2, 2);
+    
+    Node n1 = service.GetVehiclePoint(0);
+    Node n2 = service.GetVehiclePoint(1);
+    Node n3 = service.GetVehiclePoint(2);
+    Node n4 = service.GetVehiclePoint(3);
+
+    GTEST_ASSERT_EQ(n1.x, 1);
+    GTEST_ASSERT_EQ(n2.x, 2);
+    GTEST_ASSERT_EQ(n3.x, 5);
+    GTEST_ASSERT_EQ(n4.x, 6);
+
+    GTEST_ASSERT_EQ(n1.y, 3);
+    GTEST_ASSERT_EQ(n2.y, 4);
+    GTEST_ASSERT_EQ(n3.y, 7);
+    GTEST_ASSERT_EQ(n4.y, 8);
 }
 
 TEST(ARRTS_VehiclePoints, AddSingleThenMultiplePoint_CheckNum)
