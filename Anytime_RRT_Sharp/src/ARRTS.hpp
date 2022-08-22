1590b4207ff3c0d45d825c27b4d4035130c29fda
Ed Carney <carneyedwardj@gmail.com>
2022-08-22 09:29:29 -0400

    Adds ability to read obstacle and vehicle data from file

--- Anytime_RRT_Sharp/src/ARRTS.hpp
+++ Anytime_RRT_Sharp/src/ARRTS.hpp
@@ -36,12 +36,14 @@ class DLL_EXPORT ArrtsService
 
         void AddObstacle(double x, double y, double r);
         void AddObstacles(const double* x, const double* y, const double* r, int numObs);
+        void AddObstaclesFromFile(FILE* file);
         Obstacle* GetObstacles();
         Obstacle GetObstacle(int i);
         int GetNumObstacles();
 
         void AddVehiclePoint(double x, double y);
         void AddVehiclePoints(const double* x, const double *y, int numPoints);
+        void AddVehiclePointsFromFile(FILE* file);
         Node* GetVehiclePoints();
         Node GetVehiclePoint(int i);
         int GetNumVehiclePoints();
