1a67d49c3741e1a6681b9d9f745ff5f184e5f455
Ed Carney <carneyedwardj@gmail,com>
2022-08-26 09:44:42 -0400

    Updates workspace graph to use vectors

--- Anytime_RRT_Sharp/src/tests/ObstacleTests.hpp
+++ Anytime_RRT_Sharp/src/tests/ObstacleTests.hpp
@@ -30,82 +30,82 @@ TEST(Obstacle, PointIntersect_DoesIntersect)
 {
     Obstacle o(1, 1, 5);
     Point p = { 2, 3 };
-    EXPECT_TRUE(o.Intersects(p));
+    EXPECT_TRUE(o.intersects(p));
 }
 
 TEST(Obstacle, PointIntersect_DoesNotIntersect)
 {
     Obstacle o(1, 1, 5);
     Point p = { 8, -7 };
-    EXPECT_FALSE(o.Intersects(p));
+    EXPECT_FALSE(o.intersects(p));
 }
 
 TEST(Obstacle, CircleIntersect_DoesIntersectPoint)
 {
     Obstacle o(1, 1, 5);
     Circle c = { 2, 2, 1 };
-    EXPECT_TRUE(o.Intersects(c));
+    EXPECT_TRUE(o.intersects(c));
 }
 
 TEST(Obstacle, CircleIntersect_DoesIntersectRadius)
 {
     Obstacle o(1, 1, 5);
     Circle c = { 7, 0, 5 };
-    EXPECT_TRUE(o.Intersects(c));
+    EXPECT_TRUE(o.intersects(c));
 }
 
 TEST(Obstacle, CircleIntersect_DoesNotIntersect)
 {
     Obstacle o(1, 1, 5);
     Circle c = { -5, -5, 1 };
-    EXPECT_FALSE(o.Intersects(c));
+    EXPECT_FALSE(o.intersects(c));
 }
 
 TEST(Obstacle, RectagleIntersect_FullyContained)
 {
     Obstacle o(3, 3, 1);
     Rectangle r = { { 1, 1 }, { 5, 5 } };
-    EXPECT_TRUE(o.Intersects(r));
+    EXPECT_TRUE(o.intersects(r));
 }
 
 TEST(Obstacle, RectagleIntersect_PointContained)
 {
     Obstacle o(2, 2, 4);
     Rectangle r = { { 1, 1 }, { 5, 5 } };
-    EXPECT_TRUE(o.Intersects(r));
+    EXPECT_TRUE(o.intersects(r));
 }
 
 TEST(Obstacle, RectagleIntersect_PointNotContainedRadiusContained_Left)
 {
     Obstacle o(0, 3, 3);
     Rectangle r = { { 1, 1 }, { 5, 5 } };
-    EXPECT_TRUE(o.Intersects(r));
+    EXPECT_TRUE(o.intersects(r));
 }
 
 TEST(Obstacle, RectagleIntersect_PointNotContainedRadiusContained_Right)
 {
     Obstacle o(6, 3, 3);
     Rectangle r = { { 1, 1 }, { 5, 5 } };
-    EXPECT_TRUE(o.Intersects(r));
+    EXPECT_TRUE(o.intersects(r));
 }
 
 TEST(Obstacle, RectagleIntersect_PointNotContainedRadiusContained_Top)
 {
     Obstacle o(3, 6, 3);
     Rectangle r = { { 1, 1 }, { 5, 5 } };
-    EXPECT_TRUE(o.Intersects(r));
+    EXPECT_TRUE(o.intersects(r));
 }
 
 TEST(Obstacle, RectagleIntersect_PointNotContainedRadiusContained_Bottom)
 {
     Obstacle o(3, 0, 3);
     Rectangle r = { { 1, 1 }, { 5, 5 } };
-    EXPECT_TRUE(o.Intersects(r));
+    EXPECT_TRUE(o.intersects(r));
 }
 
 TEST(Obstacle, RectagleIntersect_DoesNotIntersect)
 {
     Obstacle o(-3, -3, 3);
     Rectangle r = { { 1, 1 }, { 5, 5 } };
-    EXPECT_FALSE(o.Intersects(r));
+    EXPECT_FALSE(o.intersects(r));
 }
