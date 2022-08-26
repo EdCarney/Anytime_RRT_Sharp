9651e305495df05fd8f5a0d3b11e3a0f41ecc3f2
Ed Carney <carneyedwardj@gmail,com>
2022-08-26 09:16:40 -0400

    Updates method naming for Obstacle

--- Anytime_RRT_Sharp/src/Obstacle.hpp
+++ Anytime_RRT_Sharp/src/Obstacle.hpp
@@ -6,19 +6,15 @@
 
 class Obstacle : public Circle
 {
-    void buildObstacle();
+    void _buildObstacle();
 
     public:
         Obstacle();
-        Obstacle(double x, double y, double r);
-        Obstacle(Point pos, double r);
-        bool Intersects(Point position);
-        bool Intersects(Circle circle);
-        bool Intersects(Rectangle rect);
-        double GetX();
-        double GetY();
-        double GetRadius();
-
+        Obstacle(double x, double y, double radius);
+        Obstacle(Point p, double radius);
+        bool Intersects(Point p);
+        bool Intersects(Circle c);
+        bool Intersects(Rectangle r);
 };
 
 #endif
