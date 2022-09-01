ed27de1e85345e091930fb44f576789368ca34b8
Ed Carney <carneyedwardj@gmail.com>
2022-08-31 23:38:04 -0400

    Pass by ref for slightly better perf and removing unnecessary variables

--- Anytime_RRT_Sharp/src/RRT_Sharp.hpp
+++ Anytime_RRT_Sharp/src/RRT_Sharp.hpp
@@ -13,7 +13,7 @@ ConfigspaceNode calcGateNode(double xPosition, double yPosition, double gateOrie
 
 // iterates through remainingNodes, determines if the addedNode is a better (cheaper) parent, and
 // rewires the graphs if it is.
-void rewireRemainingNodes(ConfigspaceGraph& G_configspace, WorkspaceGraph& G_workspace, vector<ConfigspaceNode> remainingNodes, ConfigspaceNode addedNode);
+void rewireRemainingNodes(ConfigspaceGraph& G_configspace, WorkspaceGraph& G_workspace, vector<ConfigspaceNode>& remainingNodes, ConfigspaceNode addedNode);
 
 // compares two nodes to determine which has a cheaper cost-to-go;
 // returns true if nodeA is cheaper, and false otherwise
@@ -22,7 +22,7 @@ bool compareNodes(ConfigspaceNode nodeA, ConfigspaceNode nodeB, ConfigspaceGraph
 // checks if any of the newNode's (safe) neighbors function as a better (cheaper) parent than
 // newNode's current parent, and updates newNode if it is (note this does NOT rewire the graphs
 // on this update, and should only be used PRIOR to adding newNode to the graphs)
-vector<ConfigspaceNode> tryConnectToBestNeighbor(ConfigspaceGraph& G_configspace, WorkspaceGraph& G_workspace, vector<ConfigspaceNode> safeNearestNeighbors, ConfigspaceNode& newNode, ConfigspaceNode& parentNode);
+void tryConnectToBestNeighbor(ConfigspaceGraph& G_configspace, WorkspaceGraph& G_workspace, vector<ConfigspaceNode>& neighbors, ConfigspaceNode& newNode, ConfigspaceNode& parentNode);
 
 // iterates through all nodes in the graphs and finds the node within
 // the goal region with the lowest cost-to-go
