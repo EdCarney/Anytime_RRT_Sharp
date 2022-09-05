260a4e64dff779f518602cd38b929667a0d163a1
EdCarney <carneyedwardj@gmail.com>
2022-09-05 19:25:47 -0400

    Adds missing references for compiling on Debian

--- Anytime_RRT_Sharp/src/Vehicle.hpp
+++ Anytime_RRT_Sharp/src/Vehicle.hpp
@@ -1,5 +1,7 @@
 #include <cstddef>
 #include <math.h>
+#include <stdexcept>
+#include <stdio.h>
 #include <vector>
 #include "cppshrhelp.hpp"
 #include "Geometry.hpp"
@@ -11,31 +13,30 @@ using namespace std;
 
 class DLL_EXPORT Vehicle
 {
-    vector<Point> _nodes;            // an array of all nodes for the vehicle
-    vector<Point> _offsetNodes;      // an array of offset values loaded on vehicle initialization
-    Point _centroid;                 // centroid of vehicle relative to the offset nodes
-    State _state;                    // state (x, y, theta) of the vehicle
-    double _boundingRadius;          // the radius of a ball circumscribing the vehicle; used for quick collision checking
-    void _updateOffsetParams();      // updates centroid and bounding radius from offset nodes
+    vector<Point> _nodes;       // an array of all nodes for the vehicle
+    vector<Point> _offsetNodes; // an array of offset values loaded on vehicle initialization
+    Point _centroid;            // centroid of vehicle relative to the offset nodes
+    State _state;               // state (x, y, theta) of the vehicle
+    double _boundingRadius;     // the radius of a ball circumscribing the vehicle; used for quick collision checking
+    void _updateOffsetParams(); // updates centroid and bounding radius from offset nodes
     void _calculateBoundingRadius();
     void _calculateCentroid();
     void _buildVehicle();
 
-    public:
-        Vehicle();
-        Vehicle(vector<double> x, vector<double> y);
-        Vehicle(FILE* file);
-        void updateState(State newState);
-        void addOffsetNode(double x, double y);
-        void addOffsetNodes(vector<double> x, vector<double> y);
-        void addOffsetNodesFromFile(FILE* file);
-        vector<Point> nodes();
-        Point nodes(int i);
-        vector<Point> offsetNodes();
-        Point offsetNodes(int i);
-        State state();
-        double boundingRadius();
-
+public:
+    Vehicle();
+    Vehicle(vector<double> x, vector<double> y);
+    Vehicle(FILE *file);
+    void updateState(State newState);
+    void addOffsetNode(double x, double y);
+    void addOffsetNodes(vector<double> x, vector<double> y);
+    void addOffsetNodesFromFile(FILE *file);
+    vector<Point> nodes();
+    Point nodes(int i);
+    vector<Point> offsetNodes();
+    Point offsetNodes(int i);
+    State state();
+    double boundingRadius();
 };
 
 #endif
