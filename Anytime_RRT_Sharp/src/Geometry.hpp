1f93fdf5810ea133f03fda7ba1b72b8ec7604835
Ed Carney <carneyedwardj@gmail.com>
2022-09-12 23:48:47 -0400

    Adds some operator overloading for easy Vector/Point manipulation

--- Anytime_RRT_Sharp/src/Geometry.hpp
+++ Anytime_RRT_Sharp/src/Geometry.hpp
@@ -21,11 +21,12 @@ class DLL_EXPORT Vector
     public:
         Vector();
         Vector(double x, double y, double z);
+        Vector operator*(double val) const;
         double x() const;
         double y() const;
         double z() const;
         double magnitude() const;
-        double dot(Vector& v) const;
+        double dot(const Vector& v) const;
 };
 
 class DLL_EXPORT Point
@@ -36,7 +37,9 @@ class DLL_EXPORT Point
     public:
         Point();
         Point(double x, double y, double z);
-        Vector operator-(Point& p) const;
+        Point operator-(const Vector& v) const;
+        Point operator+(const Vector& v) const;
+        Vector operator-(const Point& p) const;
         double x() const;
         double y() const;
         double z() const;
@@ -69,6 +72,7 @@ class DLL_EXPORT Plane
         Plane(Point p, Vector normal);
         Point point() const;
         Vector normal() const;
+        Point getIntersectionPoint(Line line) const;
 };
 
 class DLL_EXPORT Sphere : public Point, public HasVolume
