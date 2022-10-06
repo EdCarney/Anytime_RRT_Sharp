3500fcb4a6e97105bb2640831a42768b3b52ea7a
Ed Carney <carneyedwardj@gmail.com>
2022-10-06 07:01:34 -0400

    Initial updates to add method to generate dubins path leading to a node

--- Anytime_RRT_Sharp/src/ConfigspaceNode.hpp
+++ Anytime_RRT_Sharp/src/ConfigspaceNode.hpp
@@ -1,12 +1,17 @@
+#include <vector>
 #include "Geometry2D.hpp"
 #include "Geometry3D.hpp"
+#include "Dubins3d/src/DubinsManeuver3d.hpp"
+
+using namespace std;
 
 #ifndef CONFIGSPACE_NODE_H
 #define CONFIGSPACE_NODE_H
 
 class ConfigspaceNode : public GraphNode
 {
-    double _cost;
+    double _cost, _pathLength;
+    vector<State> _pathTo;
     void _buildConfigspaceNode();
     void _buildConfigspaceNode(GraphNode node);
     void _buildConfigspaceNode(double x, double y, double z, double theta, double rho, int id, int parentId, double cost);
@@ -16,7 +21,9 @@ class ConfigspaceNode : public GraphNode
         ConfigspaceNode(GraphNode node);
         ConfigspaceNode(double x, double y, double z, double theta, double rho, int id, int parentId, double cost);
         double cost() const;
+        double pathLength() const;
         void setCost(double cost);
+        void generatePathFrom(State parentState);
 };
 
 #endif //CONFIGSPACE_NODE_H
