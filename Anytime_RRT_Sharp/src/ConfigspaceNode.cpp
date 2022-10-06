59a80e0ba8957823907a9daff334fd724b226b8a
Ed Carney <carneyedwardj@gmail.com>
2022-10-06 07:01:34 -0400

    Initial updates to add method to generate dubins path leading to a node

--- Anytime_RRT_Sharp/src/ConfigspaceNode.cpp
+++ Anytime_RRT_Sharp/src/ConfigspaceNode.cpp
@@ -33,12 +33,35 @@ ConfigspaceNode::ConfigspaceNode(double x, double y, double z, double theta, dou
     _buildConfigspaceNode(x, y, z, theta, rho, id, parentId, cost);
 }
 
-double ConfigspaceNode::cost() const
-{
-    return _cost;
-}
+double ConfigspaceNode::cost() const { return _cost; }
 
-void ConfigspaceNode::setCost(double cost)
+double ConfigspaceNode::pathLength() const { return _pathLength; }
+
+void ConfigspaceNode::setCost(double cost) { _cost = cost; }
+
+void ConfigspaceNode::generatePathFrom(State parentState)
 {
-    _cost = cost;
+    State3d qi { parentState.x(), parentState.y(), parentState.z(), parentState.theta(), parentState.rho() };
+    State3d qf { x(), y(), z(), theta(), rho() };
+
+    double rhoMin = 10;
+    tuple<double, double> pitchLims = { -15.0 * M_PI / 180.0, 15.0 * M_PI / 180.0 };
+
+    DubinsManeuver3d maneuver(qi, qf, rhoMin, pitchLims);
+
+    _pathLength = maneuver.length();
+
+    if (_pathLength < 0)
+        // no path possible
+        return;
+    
+    vector<State3d> states = maneuver.computeSampling(100);
+    _pathTo.resize(states.size());
+
+    State temp;
+    for (int i = 0; i < states.size(); ++i)
+    {
+        temp = State(states.at(i).x, states.at(i).y, states.at(i).z, states.at(i).theta, states.at(i).gamma);
+        _pathTo[i] = temp;
+    }
 }
