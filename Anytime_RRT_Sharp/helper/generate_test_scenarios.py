28c16c059c7c7b11cba34b094640077cd23a3319
Ed Carney <carneyedwardj@gmail.com>
2022-10-11 13:21:36 -0400

    Updates helper scripts for generating random pitch and yaw values

--- Anytime_RRT_Sharp/helper/generate_test_scenarios.py
+++ Anytime_RRT_Sharp/helper/generate_test_scenarios.py
@@ -6,10 +6,10 @@ from plotting_tools import generatePlot
 
 GOAL_RADIUS = 2.5
 
-def generatePointAvoidingObstacles(xRange: tuple, yRange: tuple, zRange: tuple, sphereObs: list[Sphere], rectanObs: list[Rectangle]) -> Point:
-    point = Point.generateRandom(xRange, yRange, zRange)
+def generatePointAvoidingObstacles(xRange: tuple, yRange: tuple, zRange: tuple, pitchRangeDeg: tuple, yawRangeDeg: tuple, sphereObs: list[Sphere], rectanObs: list[Rectangle]) -> Point:
+    point = Point.generateRandom(xRange, yRange, zRange, pitchRangeDeg, yawRangeDeg)
     while (any(x.intersects(point) for x in sphereObs) or any(x.intersects(point) for x in rectanObs)):
-        point = Point.generateRandom(xRange, yRange, zRange)
+        point = Point.generateRandom(xRange, yRange, zRange, pitchRangeDeg, yawRangeDeg)
     return point
 
 def writeOutputData(start: Point, goal: Point, spheres: list[Sphere], rectangles: list[Rectangle]) -> None:
@@ -30,23 +30,25 @@ def writeOutputData(start: Point, goal: Point, spheres: list[Sphere], rectangles
         obsFile.write("RECTANGLE {0} {1} {2} {3} {4} {5}\n".format(r.minPoint.x, r.minPoint.y, r.minPoint.z, r.maxPoint.x, r.maxPoint.y, r.maxPoint.z))
     obsFile.close()
 
-    stateFile.write("FORMAT: (startX startY startZ startTheta) (goalX goalY goalZ goalTheta)\n")
-    stateFile.write("{0} {1} {2} 0.0\n".format(start.x, start.y, start.z))
-    stateFile.write("{0} {1} {2} 0.0 {3}\n".format(goal.x, goal.y, goal.z, GOAL_RADIUS))
+    stateFile.write("FORMAT: (startX startY startZ startPitch startYaw) (goalX goalY goalZ goalTheta goalYaw goalRadius)\n")
+    stateFile.write("{0} {1} {2} {3} {4}\n".format(start.x, start.y, start.z, start.pitch, start.yaw))
+    stateFile.write("{0} {1} {2} {3} {4} {5}\n".format(goal.x, goal.y, goal.z, goal.pitch, goal.yaw, GOAL_RADIUS))
     stateFile.close()
 
 ### define ranges for data generation ###
 numSphereObsRange = (5, 20)
 numRectanObsRange = (5, 30)
-xRange = (0, 150)
-yRange = (0, 150)
+xRange = (0, 750)
+yRange = (0, 750)
 sphereObsZRange = (40, 120)
 sphereObsRadiusRange = (1, 10)
 rectanObsZRange = (0, 0)
 rectanObsHeightRange = (10, 80)
-rectanObsWidthRange = (5, 15)
-startZRange = (100, 160)
+rectanObsWidthRange = (10, 30)
+startZRange = (150, 200)
 goalZRange = (0, 5)
+pitchRangeDeg = (-5, 5)
+yawRangeDeg = (0, 360)
 #########################################
 
 numSphereObs = randrange(numSphereObsRange[0], numSphereObsRange[1])
@@ -55,8 +57,8 @@ numRectanObs = randrange(numRectanObsRange[0], numRectanObsRange[1])
 sphereObs = [Sphere.generateRandom(xRange, yRange, sphereObsZRange, sphereObsRadiusRange) for _ in range(numSphereObs)]
 rectanObs = [Rectangle.generateRandom(xRange, yRange, rectanObsZRange, rectanObsWidthRange, rectanObsHeightRange) for _ in range(numRectanObs)]
 
-startPoint = generatePointAvoidingObstacles(xRange, yRange, startZRange, sphereObs, rectanObs)
-goalPoint = generatePointAvoidingObstacles(xRange, yRange, goalZRange, sphereObs, rectanObs)
+startPoint = generatePointAvoidingObstacles(xRange, yRange, startZRange, pitchRangeDeg, yawRangeDeg, sphereObs, rectanObs)
+goalPoint = generatePointAvoidingObstacles(xRange, yRange, goalZRange, pitchRangeDeg, yawRangeDeg, sphereObs, rectanObs)
 
 writeOutputData(startPoint, goalPoint, sphereObs, rectanObs)
 generatePlot(startPoint, goalPoint, GOAL_RADIUS, sphereObs, rectanObs)
