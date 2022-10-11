21fef8ddacef5a47ed14a197324d7031da6ce133
Ed Carney <carneyedwardj@gmail.com>
2022-10-11 13:21:36 -0400

    Updates helper scripts for generating random pitch and yaw values

--- Anytime_RRT_Sharp/helper/basic_geometry.py
+++ Anytime_RRT_Sharp/helper/basic_geometry.py
@@ -2,20 +2,31 @@ import math
 from random import uniform
 
 class Point:
-    def __init__(self, x: float, y: float, z: float) -> None:
+    def __init__(self, x: float, y: float, z: float, pitch: float = 0, yaw: float = 0) -> None:
         self.x = x
         self.y = y
         self.z = z
+        self.pitch = pitch
+        self.yaw = yaw
 
     @classmethod
-    def generateRandom(cls, xRange: tuple, yRange: tuple, zRange: tuple) -> 'Point':
+    def generateRandom(cls, xRange: tuple, yRange: tuple, zRange: tuple, pitchRangeDeg: tuple, yawRangeDeg: tuple) -> 'Point':
         x = uniform(xRange[0], xRange[1])
         y = uniform(yRange[0], yRange[1])
         z = uniform(zRange[0], zRange[1])
-        return cls(x, y, z)
+        pitch = uniform(pitchRangeDeg[0] * (math.pi / 180), pitchRangeDeg[1] * (math.pi / 180))
+        yaw = uniform(yawRangeDeg[0] * (math.pi / 180), yawRangeDeg[1] * (math.pi / 180))
+        return cls(x, y, z, pitch, yaw)
+
+    @classmethod
+    def generateRandomBasic(cls, xRange: tuple, yRange: tuple, zRange: tuple) -> 'Point':
+        x = uniform(xRange[0], xRange[1])
+        y = uniform(yRange[0], yRange[1])
+        z = uniform(zRange[0], zRange[1])
+        return cls(x, y, z, 0, 0)
 
     def __str__(self) -> str:
-        return  "({0}, {1}, {2})".format(self.x, self.y, self.z)
+        return  "({0}, {1}, {2}, {3}, {4})".format(self.x, self.y, self.z, self.pitch, self.yaw)
 
 class Rectangle:
     def __init__(self, minPoint: Point, maxPoint: Point) -> None:
@@ -33,7 +44,7 @@ class Rectangle:
         xWidth = uniform(widthRange[0], widthRange[1])
         yWidth = uniform(widthRange[0], widthRange[1])
         height = uniform(heightRange[0], heightRange[1])
-        minPoint = Point.generateRandom(xRange, yRange, zRange)
+        minPoint = Point.generateRandomBasic(xRange, yRange, zRange)
         maxPoint = Point(minPoint.x + xWidth, minPoint.y + yWidth, minPoint.z + height)
         return cls(minPoint, maxPoint)
 
@@ -58,7 +69,7 @@ class Sphere(Point):
 
     @classmethod
     def generateRandom(cls, xRange: tuple, yRange: tuple, zRange: tuple, rRange: tuple) -> 'Sphere':
-        point = Point.generateRandom(xRange, yRange, zRange)
+        point = Point.generateRandomBasic(xRange, yRange, zRange)
         radius = uniform(rRange[0], rRange[1])
         return cls(point, radius)
 
