aa4f3b474be5b97a33ba23a30dcf5639f48d97ba
Ed Carney <carneyedwardj@gmail.com>
2022-10-15 17:02:06 -0400

    Adds normalization for Python plotting tools

--- Anytime_RRT_Sharp/helper/display_search_cppLib.py
+++ Anytime_RRT_Sharp/helper/display_search_cppLib.py
@@ -1,4 +1,6 @@
 import os, sys
+from typing import Tuple
+from math import inf
 import matplotlib.pyplot as plt
 from basic_geometry import *
 from plotting_tools import *
@@ -63,6 +65,71 @@ def plotStates(ax, folder: str) -> None:
     goalRadius = float(goalLine[4])
     plotStartGoalStates(ax, start, goal, goalRadius)
 
+def getAxes(folder: str) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
+    minLims, maxLims = getLimits(folder)
+    axisSizes = list(map(lambda x: x[0] - x[1], zip(maxLims, minLims)))
+    maxAxisSize = max(axisSizes)
+
+    minAxisLims = list(map(lambda val: val[0] - (maxAxisSize - val[1]) / 2, zip(minLims, axisSizes)))
+    maxAxisLims = list(map(lambda val: val[0] + (maxAxisSize - val[1]) / 2, zip(maxLims, axisSizes)))
+
+    # set z axis to have zero as bottom
+    maxAxisLims[2] = maxAxisSize
+    minAxisLims[2] = 0
+
+    return (minAxisLims, maxAxisLims)
+
+def getLimits(folder: str) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
+    minStateLims, maxStateLims = getStateLimits(folder)
+    minPathLims, maxPathLims = getPathLimits(folder)
+    minObstacleLims, maxObstacleLims = getObstacleLimits(folder)
+    minLims = list(map(min, zip(minStateLims, minPathLims, minObstacleLims)))
+    maxLims = list(map(max, zip(maxStateLims, maxPathLims, maxObstacleLims)))
+    return (minLims, maxLims)
+
+def getStateLimits(folder: str) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
+    stateFile = os.path.join(folder, STATE_FILE_NAME)
+    lines = __getSplitFileLines(stateFile)
+    maxVals = (-inf, -inf, -inf)
+    minVals = (inf, inf, inf)
+    for line in lines:
+        if "FORMAT:" in line: continue
+        valItrs = list(zip(map(float, line), range(3)))
+        maxVals = list(map(lambda valItr: max(valItr[0], maxVals[valItr[1]]), valItrs))
+        minVals = list(map(lambda valItr: min(valItr[0], minVals[valItr[1]]), valItrs))
+    return (minVals, maxVals)
+
+def getPathLimits(folder: str) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
+    pathFile = os.path.join(folder, PATH_FILE_NAME)
+    lines = __getSplitFileLines(pathFile)
+    maxVals = (-inf, -inf, -inf)
+    minVals = (inf, inf, inf)
+    for line in lines:
+        if "FORMAT:" in line: continue
+        valItrs = list(zip(map(float, line), range(3)))
+        maxVals = list(map(lambda valItr: max(valItr[0], maxVals[valItr[1]]), valItrs))
+        minVals = list(map(lambda valItr: min(valItr[0], minVals[valItr[1]]), valItrs))
+    return (minVals, maxVals)
+
+def getObstacleLimits(folder: str) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
+    obstacleFile = os.path.join(folder, OBSTACLE_FILE_NAME)
+    lines = __getSplitFileLines(obstacleFile)
+    maxVals = (-inf, -inf, -inf)
+    minVals = (inf, inf, inf)
+    for line in lines:
+        if "FORMAT:" in line: continue
+        if (line[0] == "SPHERE"):
+            radius = float(line[4])
+            valItrs = list(zip(map(float, line[1:4]), range(3)))
+            maxVals = list(map(lambda valItr: max(valItr[0] + radius, maxVals[valItr[1]]), valItrs))
+            minVals = list(map(lambda valItr: min(valItr[0] - radius, minVals[valItr[1]]), valItrs))
+        elif (line[0] == "RECTANGLE"):
+            minValIts = list(zip(map(float, line[1:4]), range(3)))
+            maxValIts = list(zip(map(float, line[4:7]), range(3)))
+            maxVals = list(map(lambda valItr: max(valItr[0], maxVals[valItr[1]]), maxValIts))
+            minVals = list(map(lambda valItr: min(valItr[0], minVals[valItr[1]]), minValIts))
+    return (minVals, maxVals)
+
 if (len(sys.argv) < 2):
     print("ERROR: at least one test data folder must be specified. Exiting...")
     exit()
@@ -72,6 +139,12 @@ folder = sys.argv[1]
 fig = plt.figure()
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 ax = fig.add_subplot(projection='3d')
+
+axesBottom, axesTop = getAxes(folder)
+
+ax.set_xlim(axesBottom[0], axesTop[0])
+ax.set_ylim(axesBottom[1], axesTop[1])
+ax.set_zlim(axesBottom[2], axesTop[2])
 
 plotPath(ax, folder)
 plotFullPath(ax, folder)
