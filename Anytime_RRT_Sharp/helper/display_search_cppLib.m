497b4436a7e31cbe8cce707aff8aee76f75c1262
Ed Carney <carneyedwardj@gmail.com>
2022-11-19 20:07:20 -0500

    Removes a lot of unnecessary plotting for matlab 3D implementation. Also adds axis limits to make them equal.

--- Anytime_RRT_Sharp/helper/display_search_cppLib.m
+++ Anytime_RRT_Sharp/helper/display_search_cppLib.m
@@ -13,68 +13,39 @@ clear all
 close all
 clc
 
-folder = "testData_20221009165702"; %% change this
+folder = "testData_paper_1_Dubins3d"; %% change this
 
-node_file = fullfile(folder, 'nodes.txt');
-edge_file = fullfile(folder, 'edges.txt');
 path_file = fullfile(folder, 'full_output_path.txt');
-search_tree_file = fullfile(folder, 'search_tree.txt');
+path_node_file = fullfile(folder, 'output_path.txt');
 obstacles_file = fullfile(folder, 'obstacles.txt');
 state_file = fullfile(folder, 'states.txt');
 
-nodes = readmatrix(node_file);
-edges_raw = readmatrix(edge_file);
 path = readmatrix(path_file);
-search_tree_raw = readmatrix(search_tree_file);
-state_raw = readmatrix(state_file);
+path_node = readmatrix(path_node_file);
+[num_path_nodes, ~] = size(path_node);
+state_raw = readcell(state_file);
 
 % specify goal region [x, y, z, radius] and start point [x, y, z]
-root_node = state_raw(1,1:3);
-uav_start = [state_raw(2,1:3), state_raw(2,5)];
-
-% a bit of data processing for faster plotting
-search_tree = nan(3*size(search_tree_raw, 1), 3);
-
-search_tree(1:3:end-2, 1) = search_tree_raw(:, 2);
-search_tree(1:3:end-2, 2) = search_tree_raw(:, 3);
-search_tree(1:3:end-2, 3) = search_tree_raw(:, 4);
-
-search_tree(2:3:end-1, 1) = search_tree_raw(:, 6);
-search_tree(2:3:end-1, 2) = search_tree_raw(:, 7);
-search_tree(2:3:end-1, 3) = search_tree_raw(:, 8);
-
-edges = nan(3*size(edges_raw, 1), 3);
-
-for i = 1:length(edges_raw(:,1))
-    edges_raw(i, 1) = find(nodes(:,6) == edges_raw(i, 1));
-    edges_raw(i, 2) = find(nodes(:,6) == edges_raw(i, 2));
-end
-
-edges(1:3:end-2, 1) = nodes(edges_raw(:, 1),1);
-edges(1:3:end-2, 2) = nodes(edges_raw(:, 1),2);
-edges(1:3:end-2, 3) = nodes(edges_raw(:, 1),3);
-edges(2:3:end-1, 1) = nodes(edges_raw(:, 2),1);
-edges(2:3:end-1, 2) = nodes(edges_raw(:, 2),2);
-edges(2:3:end-1, 3) = nodes(edges_raw(:, 2),3);
+root_node_array = double(string(state_raw(2)).split(' '));
+uav_start_array = double(string(state_raw(3)).split(' '));
+root_node = root_node_array(1:3);
+uav_start = [uav_start_array(1:3)', uav_start_array(6)];
 
 figure('Renderer', 'painters', 'Position', [50 50 600 600])
-
-% plot the goal region
-[xs,ys,zs] = sphere();
-xs = xs*uav_start(4);
-ys = ys*uav_start(4);
-zs = zs*uav_start(4);
-surf(xs + uav_start(1),ys + uav_start(2),zs + uav_start(3),'EdgeColor','none')
-
 hold on
 
-%plot3(nodes(:,1), nodes(:,2), nodes(:,3), '.k')
-%plot3(edges(:,1), edges(:,2), edges(:,3), 'k')
-%plot3(search_tree(:, 1), search_tree(:, 2), search_tree(:, 3), 'm', 'LineWidth', 0.5);
-plot3(root_node(1), root_node(2), root_node(3), 'or', 'MarkerSize', 7,...
-    'MarkerFaceColor', 'r')
-plot3(path(:,1), path(:,2), path(:,3), 'g:*', 'LineWidth', 4);
+h = zeros(1, 4);
 
+marker_size = 10;
+
+h(1) = plot3(root_node(1), root_node(2), root_node(3), 'square', 'MarkerSize', marker_size,...
+    'MarkerFaceColor', 'b', 'DisplayName', 'Initial Position');
+h(2) = plot3(uav_start(1), uav_start(2), uav_start(3), 'ob', 'MarkerSize', marker_size,...
+    'MarkerFaceColor', 'b', 'DisplayName', 'Goal Region');
+h(3) = plot3(path_node(2:num_path_nodes-1,1), path_node(2:num_path_nodes-1,2),...
+    path_node(2:num_path_nodes-1,3), '^b', 'MarkerSize', marker_size, 'MarkerFaceColor', 'b',...
+    'DisplayName', 'Intermediate Nodes');
+h(4) = plot3(path(:,1), path(:,2), path(:,3), 'g-', 'LineWidth', 3, 'DisplayName', 'Flight Path');
 
 % plot obstacles
 sphere_obstacles = [];
@@ -92,9 +63,39 @@ while ischar(line)
     line = fgetl(obs_fid);
 end
 
-title('Path Search Tree')
+legend(h, "Location","east")
 xlabel('X-Coordinate (m)')
 ylabel('Y-Coordinate (m)')
+
+% set axis to equal
+current_axis = axis;
+current_xlim = current_axis(1:2);
+current_ylim = current_axis(3:4);
+current_zlim = zlim;
+
+zlim([0 current_zlim(2)])
+
+x_diff = current_xlim(2) - current_xlim(1);
+y_diff = current_ylim(2) - current_ylim(1);
+z_diff = current_zlim(2);
+
+max_diff = max([x_diff, y_diff, z_diff]);
+
+shift_xlim = (max_diff - x_diff) / 2;
+shift_ylim = (max_diff - y_diff) / 2;
+shift_zlim = max_diff - z_diff;
+
+new_xlim = [current_xlim(1) - shift_xlim, current_xlim(2) + shift_xlim];
+new_ylim = [current_ylim(1) - shift_ylim, current_ylim(2) + shift_ylim];
+new_zlim = [0, current_zlim(2) + shift_zlim];
+
+xlim(new_xlim)
+ylim(new_ylim)
+zlim(new_zlim)
+
+view(90,0)
+grid on
+box on
 hold off
 
 
